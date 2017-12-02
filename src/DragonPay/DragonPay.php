64c3bd9cbc192add15c4c87c9f39709b9c8cc3a8
romanornr <romanornr@gmail.com>
2017-12-02 13:25:38 +0100

    code

--- src/DragonPay/DragonPay.php
+++ src/DragonPay/DragonPay.php
@@ -2,7 +2,80 @@
 
 namespace DragonPay;
 
+use BitWasp\Bitcoin\PaymentProtocol\RequestBuilder;
+
 class DragonPay
 {
 
+	/**
+	 * The vendor can choose a minimum confirmation amount
+	 * before the transaction is considered confirmed,
+	 */
+	protected $minConfirmations = 0;
+
+
+    /**
+     * The blockexplorer we will be using
+     */
+    protected $explorer;
+
+    public function __construct($explorer)
+    {
+        $this->explorer = $explorer;
+    }
+
+    public function test()
+    {
+
+    }
+
+	/**
+	 * When the payment is received but not yet confirmed this will return true
+     *
+     * @return boolean
+	 */
+	public function transactionPaid($address)
+	{
+		$transactions = $this->explorer->getTransactions($address);
+
+		return count($transactions);
+	}
+
+    /**
+     *
+     * @return boolean
+     */
+	public function transactionConfirmed($address)
+	{
+
+		// Get the first transaction
+		$transaction = $this->explorer->getTransactions($address)[0];
+
+
+		return $this->minConfirmations >= $transaction->confirmations();
+
+	}
+
+    /**
+     * Return the status of the transaction
+     *
+     * @return array
+     */
+    public function transactionStatus($address)
+    {
+
+        return [
+            'paid' => $this->transactionPaid($address),
+            'confirmed' => $this->transactionConfirmed($address)
+        ];
+
+    }
+
 }
+
+$dragon = new DragonPay(null);
+//$test = $dragon->test();
+//
+var_dump($dragon->test());
+
+

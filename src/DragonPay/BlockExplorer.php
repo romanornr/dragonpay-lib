cc64137cc1c710eef8c521b29b5f6d52684148f3
romanornr <romanornr@gmail.com>
2017-12-10 14:28:06 +0100

    add some coments

--- src/DragonPay/BlockExplorer.php
+++ src/DragonPay/BlockExplorer.php
@@ -8,16 +8,35 @@ use Mockery\Exception;
 
 class BlockExplorer {
 
+    /**
+     * @var int
+     */
     protected $minConfirmations = 3;
 
+    /**
+     * @var
+     */
     private $network;
 
+    /**
+     * @var string
+     */
     private $currency;
 
+    /**
+     * @var float
+     */
     private $totalReceived;
 
+    /**
+     * @var mixed
+     */
     private $transactionHashEndpoint;
 
+    /**
+     * BlockExplorer constructor.
+     * @param $address
+     */
     public function __construct($address)
     {
         $this->currency = 'bitcoin';
@@ -29,12 +48,23 @@ class BlockExplorer {
         $this->totalReceived = $res->total_received;
     }
 
+    /**
+     * Check if the address received as much as the orderPrice is.
+     * If so, the payment is paid
+     * @param $orderPrice
+     * @return bool
+     */
     public function isPaid($orderPrice)
     {
         if($this->totalReceived == $orderPrice && $this->transactionHashEndpoint->unconfirmed_n_tx == 0) return true;
         return false;
     }
 
+    /**
+     * If the unconfirmed number of transaction is 0
+     * it means all transactions are paid.
+     * @return bool
+     */
     public function isConfirmed()
     {
         if($this->transactionHashEndpoint->unconfirmed_n_tx ==0) return true;

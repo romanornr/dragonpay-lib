0e1abb39d3a3a589a2a53974dcbb74f318379455
romanornr <romanornr@gmail.com>
2017-12-17 14:25:12 +0100

    fix transaction table class

--- database/migrations/2017_12_10_150254_create_transaction_table.php
+++ database/migrations/2017_12_10_150254_create_transaction_table.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTransactionsTable extends Migration
+class CreateTransactionTable extends Migration
 {
     /**
      * Run the migrations.
